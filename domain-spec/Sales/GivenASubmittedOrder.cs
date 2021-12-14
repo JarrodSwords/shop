ad3d483939268057e7469dada4b9aa1e7ab970ee
Jarrod Swords <jarrod.swords@gmail.com>
2021-12-14 16:55:21 -0500

    add order

---
+++ domain-spec/Sales/GivenASubmittedOrder.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Shop.Domain.Sales;
+using Shop.Domain.Spec.Catalog;
+using Xunit;
+
+namespace Shop.Domain.Spec.Sales
+{
+    public class GivenASubmittedOrder
+    {
+        #region Core
+
+        private readonly Order _order = ObjectProvider.SubmittedOrder;
+
+        #endregion
+
+        #region Test Methods
+
+        [Fact]
+        public void WhenCancelled_ThenOrderIsCancelled()
+        {
+            _order.Cancel();
+
+            _order.IsCancelled.Should().BeTrue();
+        }
+
+        #endregion
+    }
+}
---
+++ domain/Sales/Order.cs
@@ -0,0 +1,19 @@
+﻿using Jgs.Ddd;
+
+namespace Shop.Domain.Sales
+{
+    public class Order : Entity
+    {
+        #region Public Interface
+
+        public bool IsCancelled { get; private set; }
+
+        public Order Cancel()
+        {
+            IsCancelled = true;
+            return this;
+        }
+
+        #endregion
+    }
+}
