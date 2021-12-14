554d913f28d00ea0eda0171cb2bc60c87c88630c
Jarrod Swords <jarrod.swords@gmail.com>
2021-12-14 17:14:16 -0500

    refactor

---
+++ domain-spec/Sales/GivenASubmittedOrder/WhenCancelled.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Shop.Domain.Sales;
+using Shop.Domain.Spec.Catalog;
+using Xunit;
+
+namespace Shop.Domain.Spec.Sales.GivenASubmittedOrder
+{
+    public class WhenCancelled
+    {
+        #region Core
+
+        private readonly Order Order = ObjectProvider.SubmittedOrder;
+
+        public WhenCancelled()
+        {
+            Order.Cancel();
+        }
+
+        #endregion
+
+        #region Test Methods
+
+        [Fact]
+        public void ThenCancellationDateIsSet()
+        {
+            Order.CancellationDate.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ThenOrderIsCancelled()
+        {
+            Order.IsCancelled.Should().BeTrue();
+        }
+
+        #endregion
+    }
+}
---
+++ domain-spec/Sales/GivenASubmittedOrder/WhenDetailsAreConfirmed.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Shop.Domain.Sales;
+using Shop.Domain.Spec.Catalog;
+using Xunit;
+
+namespace Shop.Domain.Spec.Sales.GivenASubmittedOrder
+{
+    #region Core
+
+    #endregion
+
+    #region Test Methods
+
+    public class WhenDetailsAreConfirmed
+    {
+        #region Core
+
+        private readonly Order Order = ObjectProvider.SubmittedOrder;
+
+        #endregion
+
+        #region Test Methods
+
+        [Fact]
+        public void WhenDetailsAreConfirmed_ThenOrderIsAwaitingPayment()
+        {
+            Order.ConfirmDetails();
+            Order.IsAwaitingPayment.Should().BeTrue();
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
