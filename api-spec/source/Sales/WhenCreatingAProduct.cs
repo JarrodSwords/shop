657d8c3bb741e1a4b6dcf3fbfef170a2270943cc
Jarrod Swords <jarrod.swords@gmail.com>
2021-12-20 23:30:10 -0500

    refactor post fixture

--- api-spec/source/Sales/WhenCreatingAProduct.cs
+++ api-spec/source/Sales/WhenCreatingAProduct.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http;
-using System.Net.Http.Json;
+﻿using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Shop.Sales.Services;
@@ -8,7 +7,7 @@ using Xunit;
 namespace Shop.Api.Spec.Sales
 {
     [Collection("storage")]
-    public class WhenCreatingAProduct : WebApiFixture, IAsyncLifetime
+    public class WhenCreatingAProduct : PostFixture<ProductDto>
     {
         #region Core
 
@@ -19,7 +18,6 @@ namespace Shop.Api.Spec.Sales
             $"MLC-LB{++Count:000}-STD"
         );
 
-        private HttpResponseMessage _result;
         private static ushort Count;
 
         public WhenCreatingAProduct(IntegrationTestingFactory<Startup> factory) : base(
@@ -31,31 +29,11 @@ namespace Shop.Api.Spec.Sales
 
         #endregion
 
-        #region Public Interface
-
-        public Task DisposeAsync() => Task.CompletedTask;
-
-        public async Task InitializeAsync()
-        {
-            _result = await HttpClient.PostAsJsonAsync("products", _command);
-        }
-
-        #endregion
-
         #region Test Methods
 
-        [Fact]
-        public void ThenContentIsAssigned()
+        public override async Task InitializeAsync()
         {
-            var product = _result.Content.ReadFromJsonAsync<ProductDto>().Result;
-
-            product.Should().NotBeNull();
-        }
-
-        [Fact]
-        public void ThenLocationHeaderIsAssigned()
-        {
-            _result.Headers.Location.Should().NotBeNull();
+            Result = await HttpClient.PostAsJsonAsync("products", _command);
         }
 
         [Fact]
