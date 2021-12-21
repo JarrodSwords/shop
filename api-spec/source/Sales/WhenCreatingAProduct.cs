3c5bcea75e0f96e41e887efed5cd073e491c01c8
Jarrod Swords <jarrod.swords@gmail.com>
2021-12-20 21:57:17 -0500

    refactor test

--- api-spec/source/Sales/WhenCreatingAProduct.cs
+++ api-spec/source/Sales/WhenCreatingAProduct.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Shop.Sales.Services;
 using Xunit;
@@ -6,9 +8,19 @@ using Xunit;
 namespace Shop.Api.Spec.Sales
 {
     [Collection("storage")]
-    public class WhenCreatingAProduct : WebApiFixture
+    public class WhenCreatingAProduct : WebApiFixture, IAsyncLifetime
     {
         #region Core
+
+        private readonly RegisterProduct _command = new(
+            "Each Lunch Box serves one and comes with one meat, one cheese, and accoutrements.",
+            "Lunch Box",
+            25,
+            $"MLC-LB{++Count:000}-STD"
+        );
+
+        private HttpResponseMessage _result;
+        private static ushort Count;
 
         public WhenCreatingAProduct(IntegrationTestingFactory<Startup> factory) : base(
             factory,
@@ -19,27 +31,39 @@ namespace Shop.Api.Spec.Sales
 
         #endregion
 
+        #region Public Interface
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        public async Task InitializeAsync()
+        {
+            _result = await HttpClient.PostAsJsonAsync("products", _command);
+        }
+
+        #endregion
+
         #region Test Methods
+
+        [Fact]
+        public void ThenContentIsAssigned()
+        {
+            var product = _result.Content.ReadFromJsonAsync<ProductDto>().Result;
+
+            product.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ThenLocationHeaderIsAssigned()
+        {
+            _result.Headers.Location.Should().NotBeNull();
+        }
 
         [Fact]
         public async void ThenTheProductIsRetrievable()
         {
-            var lunchBox = new RegisterProduct(
-                "Each Lunch Box serves one and comes with one meat, one cheese, and accoutrements.",
-                "Lunch Box",
-                25,
-                "MLC-LB-STD"
-            );
+            var product = await HttpClient.GetFromJsonAsync<ProductDto>($"products/{_command.Sku}");
 
-            var result = await HttpClient.PostAsJsonAsync("products", lunchBox);
-
-            result.Headers.Location.Should().NotBeNull();
-
-            var returnedProduct = result.Content.ReadFromJsonAsync<ProductDto>().Result;
-
-            var storedProduct = await HttpClient.GetFromJsonAsync<ProductDto>($"products/{returnedProduct.Sku}");
-
-            returnedProduct.Should().Be(storedProduct);
+            product.Should().NotBeNull();
         }
 
         #endregion
