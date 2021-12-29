a7721e82c720d0b67dff4525448200eebafcf1d3
Jarrod Swords <jarrod.swords@gmail.com>
2021-12-29 13:01:49 -0500

    add set price

---
+++ sales/source/Product.cs
@@ -0,0 +1,32 @@
+﻿using Jgs.Ddd;
+using Shop.Shared;
+
+namespace Shop.Sales
+{
+    public class Product : Aggregate
+    {
+        #region Creation
+
+        public Product(
+            Money price = default,
+            Id id = default
+        ) : base(id)
+        {
+            Price = price ?? Money.Zero;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public Money Price { get; private set; }
+
+        public Product Set(Money price)
+        {
+            Price = price;
+            return this;
+        }
+
+        #endregion
+    }
+}
