006119b3f05a410b1a7b4c7f87b22594371b8b6f
Jarrod Swords <jarrod.swords@gmail.com>
2022-01-03 11:00:14 -0500

    reformat

--- write/source/_migrations/20220103070408_PostVendorRefactor.cs
+++ write/source/_migrations/20220103070408_PostVendorRefactor.cs
@@ -5,82 +5,82 @@ namespace Shop.Write.source._migrations
 {
     public partial class PostVendorRefactor : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Customer",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Customer", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Order",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Subtotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Tip = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Total = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Order", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Product",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    IsBox = table.Column<bool>(type: "bit", nullable: false),
-                    IsDessert = table.Column<bool>(type: "bit", nullable: false),
-                    IsSide = table.Column<bool>(type: "bit", nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Size = table.Column<int>(type: "int", nullable: false),
-                    Sku = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    VendorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Product", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Vendor",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    SkuToken = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Vendor", x => x.Id);
-                });
-        }
+        #region Protected Interface
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Customer");
+                "Customer"
+            );
 
             migrationBuilder.DropTable(
-                name: "Order");
+                "Order"
+            );
 
             migrationBuilder.DropTable(
-                name: "Product");
+                "Product"
+            );
 
             migrationBuilder.DropTable(
-                name: "Vendor");
+                "Vendor"
+            );
         }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "Customer",
+                table => new
+                {
+                    Id = table.Column<Guid>("uniqueidentifier", nullable: false),
+                    Email = table.Column<string>("nvarchar(max)", nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_Customer", x => x.Id); }
+            );
+
+            migrationBuilder.CreateTable(
+                "Order",
+                table => new
+                {
+                    Id = table.Column<Guid>("uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>("uniqueidentifier", nullable: false),
+                    Subtotal = table.Column<decimal>("decimal(18,2)", nullable: false),
+                    Tip = table.Column<decimal>("decimal(18,2)", nullable: false),
+                    Total = table.Column<decimal>("decimal(18,2)", nullable: false)
+                },
+                constraints: table => { table.PrimaryKey("PK_Order", x => x.Id); }
+            );
+
+            migrationBuilder.CreateTable(
+                "Product",
+                table => new
+                {
+                    Id = table.Column<Guid>("uniqueidentifier", nullable: false),
+                    IsBox = table.Column<bool>("bit", nullable: false),
+                    IsDessert = table.Column<bool>("bit", nullable: false),
+                    IsSide = table.Column<bool>("bit", nullable: false),
+                    Price = table.Column<decimal>("decimal(18,2)", nullable: false),
+                    Description = table.Column<string>("nvarchar(max)", nullable: true),
+                    Name = table.Column<string>("nvarchar(max)", nullable: true),
+                    Size = table.Column<int>("int", nullable: false),
+                    Sku = table.Column<string>("nvarchar(max)", nullable: true),
+                    VendorId = table.Column<Guid>("uniqueidentifier", nullable: false)
+                },
+                constraints: table => { table.PrimaryKey("PK_Product", x => x.Id); }
+            );
+
+            migrationBuilder.CreateTable(
+                "Vendor",
+                table => new
+                {
+                    Id = table.Column<Guid>("uniqueidentifier", nullable: false),
+                    Name = table.Column<string>("nvarchar(max)", nullable: true),
+                    SkuToken = table.Column<string>("nvarchar(max)", nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_Vendor", x => x.Id); }
+            );
+        }
+
+        #endregion
     }
 }
