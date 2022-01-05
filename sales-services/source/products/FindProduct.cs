7b412a03fe133fb90910e449f79e715db7bb29e2
Jarrod Swords <jarrod.swords@gmail.com>
2022-01-05 17:32:32 -0500

    separate files

--- sales-services/source/products/FindProduct.cs
+++ sales-services/source/products/FindProduct.cs
@@ -10,36 +10,4 @@ namespace Shop.Sales.Services
 
         #endregion
     }
-
-    public record SetPrice(
-        decimal Price,
-        string Sku
-    ) : ICommand
-    {
-        public class Handler : Handler<SetPrice>
-        {
-            #region Creation
-
-            public Handler(IUnitOfWork uow) : base(uow)
-            {
-            }
-
-            #endregion
-
-            #region Public Interface
-
-            public override void Handle(SetPrice args)
-            {
-                var (price, sku) = args;
-
-                var product = Uow.Products.Find(sku);
-                product.Set(price);
-
-                Uow.Products.Update(product);
-                Uow.Commit();
-            }
-
-            #endregion
-        }
-    }
 }
