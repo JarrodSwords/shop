c56b6358b8ad857918d246186a59752c00939933
Jarrod Swords <jarrod.swords@gmail.com>
2022-01-10 09:23:35 -0500

    email valid

---
+++ shared-kernel-spec/WhenCreatingAnEmail.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Shop.Shared.Spec
+{
+    public class WhenCreatingAnEmail
+    {
+        #region Test Methods
+
+        [Theory]
+        [InlineData("jon.doe@gmail.com")]
+        [InlineData("jane.doe@gmail.com")]
+        public void ThenAValidEmailIsReturned(string input)
+        {
+            var email = new Email(input);
+
+            email.Should().NotBeNull();
+        }
+
+        #endregion
+    }
+}
