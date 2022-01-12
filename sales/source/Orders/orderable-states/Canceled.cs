678f3b031dbb0c838000906af9a75f45ed262fb0
Jarrod Swords <jarrod.swords@gmail.com>
2022-01-12 11:51:39 -0500

    cannot apply payment to canceled order

--- sales/source/Orders/orderable-states/Canceled.cs
+++ sales/source/Orders/orderable-states/Canceled.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Jgs.Functional.Explicit;
+﻿using Jgs.Functional.Explicit;
 using Shop.Shared;
 using static Shop.Shared.Error;
 
@@ -9,7 +8,9 @@ namespace Shop.Sales.Orders
     {
         #region Public Interface
 
-        public override Result<Error> ApplyPayment(Money value) => throw new NotImplementedException();
+        public override Result<Error> ApplyPayment(Money value) =>
+            InvalidOperation("Cannot apply payment to a canceled order.");
+
         public override Result<Error> Cancel() => InvalidOperation("Order already canceled.");
         public override Result<Error> Confirm() => InvalidOperation("Canceled order cannot be confirmed.");
 
