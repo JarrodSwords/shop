a1e1822dd30cc46e94589d9b20c44950fe380a4d
Jarrod Swords <jarrod.swords@gmail.com>
2022-01-15 13:04:08 -0500

    refactor tests

--- sales-spec/source/Orders/GivenAPendingOrder.cs
+++ sales-spec/source/Orders/GivenAPendingOrder.cs
@@ -2,22 +2,56 @@
 using Jgs.Ddd;
 using Shop.Sales.Orders;
 using Xunit;
+using static Shop.Shared.Error;
 
 namespace Shop.Sales.Spec.Orders
 {
     public class GivenAPendingOrder
     {
+        #region Core
+
+        private readonly Order _order;
+
+        public GivenAPendingOrder()
+        {
+            _order = OrderProvider.CreateOrder();
+            _order.Add(new LineItem(25, new Id(), 1));
+        }
+
+        #endregion
+
         #region Test Methods
+
+        [Fact]
+        public void WhenApplyingPayment_ThenReturnInvalidOperationError()
+        {
+            var error = _order.ApplyPayment(1).Error;
+
+            error.Should().Be(InvalidOperation());
+        }
+
+        [Fact]
+        public void WhenConfirmed_ThenReturnInvalidOperationError()
+        {
+            var error = _order.Confirm().Error;
+
+            error.Should().Be(InvalidOperation());
+        }
+
+        [Fact]
+        public void WhenRefundIssued_ThenReturnInvalidOperationError()
+        {
+            var error = _order.IssueRefund().Error;
+
+            error.Should().Be(InvalidOperation());
+        }
 
         [Fact]
         public void WhenSubmitted_ThenOrderIsAwaitingConfirmation()
         {
-            var order = OrderProvider.CreateOrder();
-            order.Add(new LineItem(25, new Id(), 1));
+            _order.Submit();
 
-            order.Submit();
-
-            order.Status.Should().Be(OrderStatus.AwaitingConfirmation);
+            _order.Status.Should().Be(OrderStatus.AwaitingConfirmation);
         }
 
         #endregion
--- sales-spec/source/Orders/GivenAPendingOrder_WithoutLineItems.cs
+++ sales-spec/source/Orders/GivenAPendingOrder_WithoutLineItems.cs
@@ -16,30 +16,6 @@ namespace Shop.Sales.Spec.Orders
         #region Test Methods
 
         [Fact]
-        public void WhenApplyingPayment_ThenReturnInvalidOperationError()
-        {
-            var error = _order.ApplyPayment(1).Error;
-
-            error.Should().Be(InvalidOperation());
-        }
-
-        [Fact]
-        public void WhenConfirmed_ThenReturnInvalidOperationError()
-        {
-            var error = _order.Confirm().Error;
-
-            error.Should().Be(InvalidOperation());
-        }
-
-        [Fact]
-        public void WhenRefundIssued_ThenReturnInvalidOperationError()
-        {
-            var error = _order.IssueRefund().Error;
-
-            error.Should().Be(InvalidOperation());
-        }
-
-        [Fact]
         public void WhenSubmitted_ThenReturnInvalidOperationError()
         {
             var error = _order.Submit().Error;
