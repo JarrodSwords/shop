e979a1a7f710e581c6458224aa2933338e1e53c8
Jarrod Swords <jarrod.swords@gmail.com>
2022-01-16 18:19:51 -0500

    update status & update finances

--- write/source/Order.cs
+++ write/source/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Shop.Sales.Orders;
 using DomainOrder = Shop.Sales.Orders.Order;
 
 namespace Shop.Write
@@ -14,8 +15,8 @@ namespace Shop.Write
         public Order(DomainOrder order) : base(order.Id)
         {
             CustomerId = order.CustomerId;
-            Subtotal = order.Finances.Subtotal;
-            Tip = order.Finances.Tip;
+            UpdateFinances(order.Finances);
+            UpdateStatus(order.Status);
         }
 
         #endregion
@@ -35,6 +36,32 @@ namespace Shop.Write
         public decimal Refunded { get; set; }
         public decimal Subtotal { get; set; }
         public decimal Tip { get; set; }
+
+        #endregion
+
+        #region Private Interface
+
+        private void UpdateFinances(Finances finances)
+        {
+            var (balance, paid, refunded, subtotal, tip) = finances;
+
+            Balance = balance;
+            Paid = paid;
+            Refunded = refunded;
+            Subtotal = subtotal;
+            Tip = tip;
+        }
+
+        private void UpdateStatus(OrderStatus status)
+        {
+            IsAwaitingConfirmation = status.HasFlag(OrderStatus.AwaitingConfirmation);
+            IsAwaitingFulfillment = status.HasFlag(OrderStatus.AwaitingFulfillment);
+            IsAwaitingPayment = status.HasFlag(OrderStatus.AwaitingPayment);
+            IsCanceled = status.HasFlag(OrderStatus.Canceled);
+            IsPending = status.HasFlag(OrderStatus.Pending);
+            IsRefundDue = status.HasFlag(OrderStatus.RefundDue);
+            IsRefunded = status.HasFlag(OrderStatus.Refunded);
+        }
 
         #endregion
 
