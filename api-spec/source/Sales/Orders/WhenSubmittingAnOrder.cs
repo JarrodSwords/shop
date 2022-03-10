9370f08ddd20cbe0f122c4ba5495353c07f19b57
Jarrod Swords <jarrod.swords@gmail.com>
2022-03-10 10:52:56 -0500

    refactor submit order tests

---
+++ api-spec/source/Sales/Orders/WhenSubmittingAnOrder.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Jgs.Cqrs;
+using Jgs.Functional.Explicit;
+using Shop.Sales;
+using Shop.Sales.Orders;
+using Shop.Sales.Services;
+using Shop.Shared;
+using Xunit;
+
+namespace Shop.Api.Spec.Sales.Orders
+{
+    [Collection("storage")]
+    public abstract class WhenSubmittingAnOrder : ApplicationFixture
+    {
+        #region Core
+
+        private readonly IQueryHandler<FindOrder, OrderDto> _findOrder;
+        protected readonly ICommandHandler<SubmitOrder, Result<OrderSubmitted, Error>> SubmitOrder;
+
+        protected WhenSubmittingAnOrder(IntegrationTestingFactory<Startup> factory) : base(factory)
+        {
+            _findOrder = Resolve<IQueryHandler<FindOrder, OrderDto>>();
+            SubmitOrder = Resolve<ICommandHandler<SubmitOrder, Result<OrderSubmitted, Error>>>();
+        }
+
+        #endregion
+
+        #region Test Methods
+
+        [Fact]
+        public void ThenTheOrderIsSaved()
+        {
+            var candidateOrder = new SubmitOrder(
+                "chase.elliott@hms.com",
+                LunchBoxes: 1
+            );
+
+            var orderSubmitted = SubmitOrder.Handle(candidateOrder).Value;
+            var order = _findOrder.Handle(orderSubmitted.Id);
+
+            order.Should().NotBeNull();
+        }
+
+        #endregion
+
+        public class WithARegisteredCustomer : WhenSubmittingAnOrder
+        {
+            #region Core
+
+            private readonly IQueryHandler<FetchCustomers, IEnumerable<CustomerDto>> _fetchCustomers;
+
+            public WithARegisteredCustomer(IntegrationTestingFactory<Startup> factory) : base(factory)
+            {
+                _fetchCustomers = Resolve<IQueryHandler<FetchCustomers, IEnumerable<CustomerDto>>>();
+            }
+
+            #endregion
+
+            #region Test Methods
+
+            [Theory]
+            [InlineData("william.byron@hms.com")]
+            public void ThenTheCustomerIsNotRegisteredTwice(string email)
+            {
+                var candidateOrder = new SubmitOrder(
+                    email,
+                    LunchBoxes: 1
+                );
+
+                SubmitOrder.Handle(candidateOrder);
+                SubmitOrder.Handle(candidateOrder);
+
+                var customers = _fetchCustomers.Handle(new());
+
+                customers.Should().ContainSingle(x => x.Email == email);
+            }
+
+            #endregion
+        }
+
+        public class WithAnUnregisteredCustomer : WhenSubmittingAnOrder
+        {
+            #region Core
+
+            private readonly IUnitOfWork _uow;
+
+            public WithAnUnregisteredCustomer(IntegrationTestingFactory<Startup> factory) : base(factory)
+            {
+                _uow = Resolve<IUnitOfWork>();
+            }
+
+            #endregion
+
+            #region Test Methods
+
+            [Theory]
+            [InlineData("kyle.larson@hms.com")]
+            public void ThenTheCustomerIsRegistered(string email)
+            {
+                var candidateOrder = new SubmitOrder(
+                    email,
+                    LunchBoxes: 1
+                );
+
+                SubmitOrder.Handle(candidateOrder);
+                var customerExists = _uow.Customers.Exists(email);
+
+                customerExists.Should().BeTrue();
+            }
+
+            #endregion
+        }
+    }
+}
